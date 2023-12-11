18f0f8a86867b14cb23d9e9c2c953eef0316a037
christos99 <mangas.99@windowslive.com>
2023-12-11 12:47:04 +0200

    new YAML

--- Jinja_render.py
+++ Jinja_render.py
@@ -1,17 +1,26 @@
+import os
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-# Load YAML file
-with open('/Users/christos/hummingbot-dsl/hummingbot/strategy/dsl_yaml_strategy/yaml_files/RSI_EthUsdt_Strategy.yaml', 'r') as file:
-    yaml_data = yaml.safe_load(file)
+try:
+    # Load YAML file
+    yaml_file_path = '/path/to/RSI_EthUsdt_Strategy.yaml'
+    with open(yaml_file_path, 'r') as file:
+        yaml_data = yaml.safe_load(file)
 
-# Load Jinja2 template
-env = Environment(loader=FileSystemLoader('/Users/christos/hummingbot-dsl/hummingbot/strategy/dsl_yaml_strategy/templates'))
-template = env.get_template('demo_v1.j2')
+    # Load Jinja2 template
+    template_dir = '/path/to/templates'
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template('demo_v1.j2')
 
-# Render the template with YAML data
-rendered_script = template.render(yaml=yaml_data)
+    # Render the template with YAML data
+    rendered_script = template.render(yaml=yaml_data)
 
-# Write the rendered script to a Python file
-with open('/Users/christos/hummingbot-dsl/hummingbot/strategy/dsl_yaml_strategy/output_strategies/RSI_EthUsdt_Strategy.py', 'w') as file:
-    file.write(rendered_script)
+    # Write the rendered script to a Python file
+    output_file_path = '/path/to/output/RSI_EthUsdt_Strategy.py'
+    with open(output_file_path, 'w') as file:
+        file.write(rendered_script)
+
+    print(f"Strategy script successfully written to {output_file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
