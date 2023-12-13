ec2c5fe38c7c8a1061b0e8648366ee9b2404aa34
christos99 <mangas.99@windowslive.com>
2023-12-13 22:43:49 +0200

    finally a working project

--- output_strategies/dynamic_indicator.py
+++ output_strategies/dynamic_indicator.py
@@ -1,125 +1,58 @@
 # Import necessary modules
 import pandas as pd
 from decimal import Decimal
-from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
-from hummingbot.core.data_type.common import OrderType, PositionSide
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig, CandlesFactory
+from hummingbot.strategy.directional_strategy_base import DirectionalStrategyBase
 
 # Strategy Class Definition
-class RSI_Strategy(DirectionalStrategy.):
+class RSI_Strategy(DirectionalStrategyBase):
     # Strategy Metadata
-    strategy_name = "RSI_Strategy"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Initialize market parameters
-                # Market configuration
+    strategy_name: str = "RSI_Strategy"    
+    
+    # Market configuration
     trading_pair: str = "ETH-USDT"
-    exchange: str = "binance"
+    exchange: str = "binance_perpetual"
     api_key = "your_api_key"
     api_secret = "your_api_secret"
-    order_amount_usd = Decimal("100")
-    leverage = 10
-        # Initialize trade parameters
-                # Configure the parameters for the position
+    order_amount_usd = Decimal("40")
+    leverage = 10    
     
-    stop_loss: float = 0.01
-    
-    
-    take_profit: float = 0.02
-    
-    
-    
-    
-    
-    
-    
-    
+    # Configure the parameters for the position
+    stop_loss: float = 0.0075
+    take_profit: float = 0.015
+    time_limit: int = 60
     trailing_stop_activation_delta = 0.004
-    
-    
     trailing_stop_trailing_delta = 0.001
-    
-    
-        # Initialize indicator parameters 
-            # Initialize indicator configurations
-    
-    
+
+    # Candles configuration 
+    candles = [CandlesFactory.get_candle(CandlesConfig(connector="binance_perpetual", trading_pair="ETH-USDT", interval="3m", max_records=1000))]
+
+
     # RSI Indicator Setup
-    rsi_length = 14
+    rsi_length = 7
     rsi_overbought = 70
     rsi_oversold = 30
     
-    
-    
-    # MACD Indicator Setup
-    macd_fast_length = 12
-    macd_slow_length = 26
-    macd_signal_smoothing = 9
-    
-    
-    
-    # EMA Indicator Setup
-    ema_length = 9
-    
-    
-        # Other initializations, if any
         
-    # Indicator process logic
-            # Function to process indicators
+    
+    # Function to process indicators
     def get_processed_df(self):
+
         candles_df = self.candles[0].candles_df.copy()
 
-        
-        
         # Calculate RSI
-        candles_df["RSI_14"] = ta.rsi(candles_df["close"], length=14)
-        
-        
-        
-        # Calculate MACD
-        macd = ta.macd(candles_df["close"], fast=12, slow=26, signal=9)
-        candles_df = candles_df.join(macd)
-        
-        
-        
-        # Calculate EMA
-        candles_df["EMA_9"] = ta.ema(candles_df["close"], length=9)
-        
-        
-
+        candles_df["RSI_7"] = ta.rsi(candles_df["close"], length=7)
         return candles_df
-
-    # Signal generation logic
-        
+    
+    
     def get_signal(self):
         candles_df = self.get_processed_df()
-
-        # Default signal is hold (0)
-        signal = 0
-
         # Check for buy conditions
-        
         if candles_df["RSI"].iat[-1] < 30:
-            signal = 1
-            break  # Exit loop on first true condition
-        
-        if candles_df["MACD"].iat[-1]  :
-            signal = 1
-            break  # Exit loop on first true condition
-        
-
-        # Check for sell conditions if no buy signal
-        
-
-        return signal
-
-
-
-    # Trade execution logic
-    def execute_trade(self, signal):
-        # This method will use signal generated from `get_signal` to execute trades
-        # Trade execution logic goes here
-        ...
-
-    # Additional methods and utilities
-    ...
+            return 1
+        # Check for sell conditions
+        elif candles_df["RSI"].iat[-1] > 70:
+            return -1
+        # Default to hold if no conditions met
+        else:
+            return 0
