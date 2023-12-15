a0cc1b9395d03919c905cf5384723198d5bbadd5
christos99 <mangas.99@windowslive.com>
2023-12-15 17:55:14 +0200

    improved_version_design

--- output_strategies/directional_strategy_rsi.py
+++ output_strategies/directional_strategy_rsi.py
@@ -1,10 +1,14 @@
 # Import necessary modules
 from decimal import Decimal
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig, CandlesFactory
+from typing import List
+from hummingbot.core.data_type.common import OrderType, PriceType, TradeType
+from hummingbot.core.data_type.order_candidate import OrderCandidate
+from hummingbot.core.event.events import OrderFilledEvent
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 # Strategy Class Definition
-class RSI_Strategy(ScriptStrategyBase):
+class RSI_Strategy():
     # Strategy Metadata
     strategy_name: str = "RSI_Strategy"    
     
@@ -15,44 +19,82 @@ class RSI_Strategy(ScriptStrategyBase):
     api_secret = "your_api_secret"
     order_amount_usd = Decimal("40")
     leverage = 10
-    # PMM Parameters
-    bid_spread = 
-    ask_spread = 
-    order_refresh_time = 
 
+    # PMM Parameters
+    bid_spread = 10
+    ask_spread = 10
+    order_refresh_time = 10
+    price_source = PriceType.MidPrice 
 
     # Candles configuration 
     candles = [CandlesFactory.get_candle(CandlesConfig(connector="binance_perpetual", trading_pair="ETH-USDT", interval="3m", max_records=1000))]
 
-
+    # Market initialization
+    markets = binance_perpetual: ETH-USDT
+    
+    
     # RSI Indicator Setup
-    rsi_length = 7
+    rsi_length = 14
     rsi_overbought = 70
     rsi_oversold = 30
     
     
     # This is the order order_management section 
-        
+    def on_tick(self):
+        if self.create_timestamp <= self.current_timestamp:
+            self.cancel_all_orders()
+            proposal = self.create_proposal()
+            proposal_adjusted = self.adjust_proposal_to_budget(proposal)
+            self.place_orders(proposal_adjusted)
+            self.create_timestamp = self.order_refresh_time + self.current_timestamp
 
-    # Function to process indicators
-    def get_processed_df(self):
+    def create_proposal(self) -> List[OrderCandidate]:
+        ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
+        buy_price = ref_price * Decimal(1 - self.bid_spread)
+        sell_price = ref_price * Decimal(1 + self.ask_spread)
 
-        candles_df = self.candles[0].candles_df
+        buy_order = OrderCandidate(
+            trading_pair=self.trading_pair, 
+            is_maker=True, 
+            order_type=OrderType.LIMIT,
+            order_side=TradeType.BUY, 
+            amount=Decimal(self.order_amount), 
+            price=buy_price
+        )
 
-        # Calculate RSI
-        candles_df["RSI_7"] = ta.rsi(candles_df["close"], length=7)
-        return candles_df
+        sell_order = OrderCandidate(
+            trading_pair=self.trading_pair, 
+            is_maker=False, 
+            order_type=OrderType.MARKET,
+            order_side=TradeType.SELL, 
+            amount=Decimal(self.order_amount), 
+            price=sell_price
+        )
 
-        
+        return [buy_order, sell_order]
+
+    def adjust_proposal_to_budget(self, proposal: List[OrderCandidate]) -> List[OrderCandidate]:
+        proposal_adjusted = self.connectors[self.exchange].budget_checker.adjust_candidates(proposal, all_or_none=True)
+        return proposal_adjusted
+
+    def place_orders(self, proposal: List[OrderCandidate]) -> None:
+        for order in proposal:
+            self.place_order(connector_name=self.exchange, order=order)
+
+    def place_order(self, connector_name: str, order: OrderCandidate):
+        if order.order_side == TradeType.SELL:
+            self.sell(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
+                    order_type=order.order_type, price=order.price)
+        elif order.order_side == TradeType.BUY:
+            self.buy(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
+                    order_type=order.order_type, price=order.price)
+
+    def cancel_all_orders(self):
+        for order in self.get_active_orders(connector_name=self.exchange):
+            self.cancel(self.exchange, order.trading_pair, order.client_order_id)
+
+    def did_fill_order(self, event: OrderFilledEvent):
+        msg = (f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}")
+        self.log_with_clock(logging.INFO, msg)
+        self.notify_hb_app_with_timestamp(msg)
     
-    def get_signal(self):
-        candles_df = self.get_processed_df()
-        # Check for buy conditions
-        if candles_df["RSI"].iat[-1] < 30:
-            return 1
-        # Check for sell conditions
-        elif candles_df["RSI"].iat[-1] > 70:
-            return -1
-        # Default to hold if no conditions met
-        else:
-            return 0
